4491d5d5b433b9b6395e27dd94c9820553be8ea0
Jon Seymour <jon@wildducktheories.com>
2021-07-09 17:24:47 +1000

    reimplement Vectors

--- spirals/SpiralPlotter.py
+++ spirals/SpiralPlotter.py
@@ -89,83 +89,123 @@ class StopState:
 
 class Vectors:
     """
-        This class knows how to represent the n unit vectors of the form e^i(2*pi*k/n)
-        for 0 <= k < n and the symmetries between them.
+        Instances of this class represent all the unit vectors that are rotated an integer multiple
+        of 2pi/n displaced from the real axis for all.
 
-        It also knows how to map an n-dimensional vector space down into a cross
-        product of two floor((n+1)/2) vector spaces by matching vectors
-        which are additive inverses (n is even) or complex conjugates (n is odd).
+        .coefficients
+            contains a matrix whose elements represent the real and imaginary components of the 12 vectors
 
-        Finally, it knows how to project the vector space into 2D space by
-        multiplying the id vectors with cosine and cosine coefficients of the unit
-        vectors and summing the results.
+        .id_coefficients
+            contains a matrix of coefficients which are not integer multiples of each other
+
+        .id_reduction
+            contains 2 matricies that collapses a n-element state vector into 2 smaller id
+            vectors such that none of the coefficients are integer multiples of the other
+
+        .to_id()
+            maps a state vector containing counts of unit vectors to a tuple of two vectors
+            whose elements correspond to the elements of id_coefficients
+
+        .to_xy()
+            maps a state vector containing counts of unit vectors to an xy coordinate
 
     """
     def __init__(self, n):
-        m=int((n+1)/2)
-        o=n%2
-        self.coefficients=np.round(np.array([
-            [np.cos(math.pi*2*i/n), np.sin(math.pi*2*i/n)] for i in range(0,m)
-        ]), 8)
-        self.m = m
+        PRECISION=1e-8
+
         self.n = n
-        self.o = o
-        self.x = np.zeros(shape=(m,n),dtype=int)
-        self.y = np.zeros(shape=(m,n),dtype=int)
-        self.x[0:m,0:m]=np.identity(m)
-        self.y[0:m,0:m]=np.identity(m)
-        if o == 0:
-            self.x[0:,self.m:]=-np.identity(m)
-            self.y[0:,self.m:]=-np.identity(m)
-        else:
-            self.x[1:,self.m:]= np.fliplr(np.identity(m-1))
-            self.y[1:,self.m:]=-np.fliplr(np.identity(m-1))
 
-        self.reductions = [ self._reduce(self.coefficients[:,i]) for i in [0,1]]
+        # self.coefficients
+        #
+        # Each row contains the magitudes of the real and imaginary components of the
+        # b unit vectors that partition the unit circle into sectors of angle 2pi/n radians each
+        # starting with the pure real unit vector (1+0i) as element 0 and
+        # iterating in an anti-clockwise direction
 
-        # eliminate all rows n i where the corresponding row in t is all zero
-        shrink=lambda  i,t : i[[not np.all(r) for r in (t == 0)]]
+        self.coefficients = np.round(np.array([ [math.cos(math.pi*2*i/n), math.sin(math.pi*2*i/n)] for i in range(0,n) ]), 12).transpose()
 
-        # coefficients that convert the id tuple into xy coordinates
-        self.id_coefficients = [
-            shrink(self.coefficients[:, i], self.reductions[i]) for i in [0,1]
-        ]
+        # m is a 3-dimensional transformation matrix.
+        #
+        # m[i,:,:] corresponds to ith axis of the complex plane (0=real, 1=imaginary)
+        # m[i,j,:] corresponds to the jth row of the output matrix
+        # m[i.j,k] corresponds to the contribution of the kth row of the state vector to the jth row of the output matrix
+        #
+        # The 3-dimensional matrix contains 2 nxn matrices, one for each of the real and
+        # imagiary axes. Each of the nxn matrices is initially equal to the nxn identity matrix.
+        #
+        # The matrices are progressively edited so that input vector elements whose
+        # corresponding coeeficients are integer multiples of each other can be summed into
+        # a single element of the output vector, effectively reducing the dimensionality of the
+        # output vectors.
+        #
+        # The intent of building this matrix is to allow it to be used to map all state vectors
+        # that map to the same XY coordinate to a unique state vector of reduced dimension, thereby
+        # allowing identity matching to be done in precise integer vector space, rather than in the
+        # imprecise domain of floating point vectors.
+        #
 
-        # shrink the reductions to eliminate the zeros
-        self.reductions = [
-            shrink(self.reductions[i], self.reductions[i]) for i in [0,1]
-        ]
+        # make a copy because we need to mutate it
+        _coefficients = np.copy(self.coefficients)
 
-    def _reduce(self, c):
-        l = len(c)
-        d = dict(zip(np.round(c,8), range(0, l)))
-        m = np.identity(l)
-        one_index = d[1.0] if 1.0 in d else None
-        for x in range(0, l):
-            a=np.abs(np.round(c[x],8))
-            if np.abs(np.round(a*2-1, 8)) < 1e-8:
-                if not one_index is None:
-                    m[x,x] = 0
-                    m[one_index,x] += np.sign(c[x])/2
-            elif a < 1e-8:
-                 m[x,x] = 0
-            elif a in d:
-                i=d[a]
-                if not a < 1e-8:
-                    m[x,x]=0
-                    m[i,x]=np.sign(c[x])
-        return m
+        m=np.zeros(shape=(2,n,n), dtype=int)
+        for i in range(0,len(m)):
+            m[i,0:n,0:n] = np.identity(n, dtype=int)
 
-    def to_id(self, p):
-        p = p - np.min(p[1:])
-        return (
-            tuple(np.matmul(self.reductions[0], np.matmul(self.x, p))),
-            tuple(np.matmul(self.reductions[1], np.matmul(self.y, p)))
-        )
+            c = _coefficients[i,:]
+            a = np.abs(c)
+
+            # if we have any coefficients whose absolute value is 0.5, then
+            # then we multiply unit coefficients by 0.5 and adjust the
+            # reduction matrix to compensate by muliplying the corresponding
+            # element by 2.
+            if len(np.where(np.abs(a-0.5) < PRECISION)[0]) > 0:
+                for j in np.where(np.abs(a-1.0) < PRECISION)[0]:
+                    c[j]=np.sign(c[j])*0.5
+                    a[j]=np.abs(c[j])
+                    m[i,j,j] = 2
+
+            # next, we ensure that any input rows whose coefficients have the
+            # same magnitude are summed into the same output row
+            # this will leave some rows containing zeros that need to be
+            # collapsed.
+            for k, e in enumerate(a):
+                j=np.where(np.abs(a-e)<PRECISION)[0][0]
+                if j != k or e<PRECISION:
+                    m[i,j,k]=int(np.sign(c[k]*c[j]))*np.abs(m[i,k,k])
+                    m[i,k,k]=0
+
+        # the collapse lambda reduces a matrix 'a' by eliding all rows
+        # where the corresponding row of matrix 'm' contains only zeros
+        collapse = lambda a : np.array([
+            a[i][
+                [not np.all(r) for r in m[i] == 0]
+            ] for i in (0,1)
+        ], dtype=object)
+
+        #
+        # we apply the collapse function to produce a matrix that can
+        # reduces a state vector to a reduced state vector in which all
+        # dimensions have
+        #
+        self.id_reduction = collapse(m)
+
+        #
+        # we apply the collapse function to coefficients vector to produce
+        # the set of coefficients such that set(np.unique(mp.abs(self.coefficients[i]))) =
+        #
+        self.id_coefficients = collapse(_coefficients)
 
 
     def to_xy(self, p):
-        return tuple([np.matmul(self.to_id(p)[i], self.id_coefficients[i]) for i in [0, 1]])
+        # converts the id vectors into a position on a tuple describing a point on the x-y plane
+        return tuple([ np.dot(self.id_coefficients[i], self.to_id(p)[i]) for i in (0,1) ])
+
+    def to_id(self, p):
+        # this exploits an identity that the sum of all unit vectors is 0
+        p = p - np.min(p)
+
+        # transforms the n-element state vactor into 2-tuple containing tuples of smaller dimension
+        return tuple([tuple(np.matmul(r, p)) for r in self.id_reduction])
 
 
 class VectorState:
@@ -175,9 +215,11 @@ class VectorState:
         The main difference between this class and MatrixState is that state is maintained as vectors of integers
         rather than floats and floats are only used when converting a state into an xy coordinate.
     """
-    def init(n):
+    def init(n, v=None):
         p=np.zeros(shape=(n), dtype=int)
-        return VectorState(n, p, 0, None, Vectors(n))
+        if v==None:
+            v = Vectors(n)
+        return VectorState(n, p, 0, None, v)
 
     def __init__(self, n, p, d, prev_d, vectors):
         self.n = n
